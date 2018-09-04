d185b7bcfd484932da76e477a7d566fc9be693a8
Simon Zidoree <simzo96@hotmail.com>
2018-09-04 14:15:50 +0200

    Added REST API to project. Added simple GET AllPets Request to REST API.

---
+++ Petshop.RESTAPI/Controllers/PetsController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Petshop.Core.ApplicationService;
+using Petshop.Core.Entities;
+
+namespace Petshop.RESTAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PetsController : ControllerBase
+    {
+        private readonly IPetService _petService;
+
+        public PetsController(IPetService petService)
+        {
+            _petService = petService;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<Pet>> Get()
+        {
+            return _petService.GetAllPets();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/pets
+        [HttpPost]
+        public void Post([FromBody] Pet pet)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Petshop.RESTAPI/Startup.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Petshop.Core.ApplicationService;
+using Petshop.Core.ApplicationService.Services;
+using Petshop.Core.DomainService;
+using Petshop.Infrastructure.Data;
+using Petshop.Infrastructure.Data.Repositories;
+
+namespace Petshop.RESTAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            FakeDB.InitData();
+
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddScoped<IPetRepository, PetRepository>();
+            services.AddScoped<IPetService, PetService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+//            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
